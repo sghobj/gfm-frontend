8da1f6a65d58f1099d4a0ed3dee18aec20ac1fe7
sghobj <sarahghobj@hotmail.com>
2026-02-23 20:12:46 +0100

    feat: fix styling to be more consistent
    # Conflicts:
    #	src/auth/AdminAuthProvider.tsx
    #	src/components/footer/Footer.tsx
    #	src/components/layout/HashScrollHandler.tsx
    #	src/pages/contact/sections/LocationMap.tsx
    #	src/pages/home/sections/Brands.tsx
    #	src/pages/home/sections/Certifications.tsx

--- src/pages/contact/sections/LocationMap.tsx
+++ src/pages/contact/sections/LocationMap.tsx
@@ -97,13 +97,14 @@ export const LocationMap = () => {
     return (
         <Box component="section">
             <Grid container spacing={4}>
-                <Grid size={{ lg: 4, md: 6, xs: 12 }} alignItems="center" alignContent="center">
+                <Grid size={{ lg: 4, md: 6, xs: 12 }} alignItems= "center"
+                alignContent= "center">
                     <Card
                         sx={{
                             borderRadius: 3,
                             background: "white",
                             boxShadow: "none",
-                            margin: "auto 0",
+                            margin: "auto 0"
                         }}
                     >
                         <CardHeader
