3620ab9efdd89964ffde5a8904bdb8d3aac5e137
sghobj <sarahghobj@hotmail.com>
2026-03-04 21:52:50 +0100

    feat: modify contact us page

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -218,27 +218,26 @@ export function Footer() {
                                         </Stack>
                                     )}
 
-                                    {hasNonEmptyText(effectiveEmail) &&
-                                        hasNonEmptyText(effectiveMailtoHref) && (
-                                            <Stack
-                                                direction="row"
-                                                useFlexGap
-                                                gap={1}
-                                                alignItems="center"
+                                    {hasNonEmptyText(effectiveEmail) && hasNonEmptyText(effectiveMailtoHref) && (
+                                        <Stack
+                                            direction="row"
+                                            useFlexGap
+                                            gap={1}
+                                            alignItems="center"
+                                        >
+                                            <EmailOutlinedIcon
+                                                fontSize="small"
+                                                sx={{ opacity: 0.8 }}
+                                            />
+                                            <MuiLink
+                                                href={effectiveMailtoHref}
+                                                underline="hover"
+                                                sx={{ color: "text.primary", opacity: 0.85 }}
                                             >
-                                                <EmailOutlinedIcon
-                                                    fontSize="small"
-                                                    sx={{ opacity: 0.8 }}
-                                                />
-                                                <MuiLink
-                                                    href={effectiveMailtoHref}
-                                                    underline="hover"
-                                                    sx={{ color: "text.primary", opacity: 0.85 }}
-                                                >
-                                                    {effectiveEmail}
-                                                </MuiLink>
-                                            </Stack>
-                                        )}
+                                                {effectiveEmail}
+                                            </MuiLink>
+                                        </Stack>
+                                    )}
                                 </Stack>
                             </Grid>
                         )}
